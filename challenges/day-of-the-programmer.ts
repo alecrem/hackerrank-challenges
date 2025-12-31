32a9715e759d37b57991bbbe70fab76f54bea688
Alejandro Cremades <ale@alecrem.com>
2026-01-01 01:36:21 +0900

    Add day-of-the-programmer problem

---
+++ challenges/day-of-the-programmer.ts
@@ -0,0 +1,56 @@
+// https://www.hackerrank.com/challenges/day-of-the-programmer/problem
+// npx tsx challenges/day-of-the-programmer.ts
+// Sample Input:
+// 2017
+// 13.09.2017
+
+"use strict";
+
+import { WriteStream, createWriteStream } from "fs";
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString: string = "";
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+process.stdin.on("data", function (inputStdin: string): void {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function (): void {
+  inputLines = inputString.split("\n");
+  inputString = "";
+
+  main();
+});
+
+function readLine(): string {
+  return inputLines[currentLine++];
+}
+
+/*
+ * Complete the 'dayOfProgrammer' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts INTEGER year as parameter.
+ */
+
+function dayOfProgrammer(year: number): string {
+  // Write your code here
+  return "";
+}
+
+function main() {
+  const ws: WriteStream = createWriteStream(
+    process.env["OUTPUT_PATH"] || "/dev/stdout"
+  );
+
+  const year: number = parseInt(readLine().trim(), 10);
+
+  const result: string = dayOfProgrammer(year);
+
+  ws.write(result + "\n");
+
+  ws.end();
+}
