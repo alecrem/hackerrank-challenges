379b031462a9cc58742effa6a757f8c0ac24e6a3
Alejandro Cremades <ale@alecrem.com>
2026-01-02 23:13:37 +0900

    Add O(n**2) solution to Pairs

--- challenges/pairs.ts
+++ challenges/pairs.ts
@@ -37,8 +37,17 @@ function readLine(): string {
  */
 
 function pairs(k: number, arr: number[]): number {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      const diff = Math.abs(arr[i] - arr[j]);
+      if (diff === k) {
+        count++;
+      }
+    }
+  }
   // Write your code here
-  return 0;
+  return count;
 }
 
 function main() {
