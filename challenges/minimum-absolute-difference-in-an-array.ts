4127afcabcebeb0d30b13c123b9a2b78d5195631
Alejandro Cremades <ale@alecrem.com>
2026-01-02 23:34:42 +0900

    Add minimum-absolute-difference-in-an-array unoptimized solution

---
+++ challenges/minimum-absolute-difference-in-an-array.ts
@@ -0,0 +1,66 @@
+// https://www.hackerrank.com/challenges/minimum-absolute-difference-in-an-array/problem
+// npx tsx challenges/minimum-absolute-difference-in-an-array.ts
+// printf "3\n3 -7 0" | npx tsx challenges/minimum-absolute-difference-in-an-array.ts
+
+"use strict";
+
+import { WriteStream, createWriteStream } from "fs";
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString: string = "";
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+process.stdin.on("data", function (inputStdin: string): void {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function (): void {
+  inputLines = inputString.split("\n");
+  inputString = "";
+
+  main();
+});
+
+function readLine(): string {
+  return inputLines[currentLine++];
+}
+
+/*
+ * Complete the 'minimumAbsoluteDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function minimumAbsoluteDifference(arr: number[]): number {
+  const numSet = new Set(arr);
+  const diffs = new Array();
+  for (const num of arr) {
+    const numSetRest = new Set(numSet);
+    numSetRest.delete(num);
+    const numDiffs = Array.from(numSetRest).map((n) => Math.abs(n - num));
+    diffs.push(Math.min(...numDiffs));
+  }
+  return Math.min(...diffs);
+}
+
+function main() {
+  const ws: WriteStream = createWriteStream(
+    process.env["OUTPUT_PATH"] || "/dev/stdout"
+  );
+
+  const n: number = parseInt(readLine().trim(), 10);
+
+  const arr: number[] = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((arrTemp) => parseInt(arrTemp, 10));
+
+  const result: number = minimumAbsoluteDifference(arr);
+
+  ws.write(result + "\n");
+
+  ws.end();
+}
